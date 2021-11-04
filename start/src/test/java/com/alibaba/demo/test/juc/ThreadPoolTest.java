27e7f8bf09cc0c4fe17ffd7639a0efd089f182e1
byg <byg184244735@126.com>
2021-11-05 07:33:28 +0800

    byg:add ParallelStreamTest

--- start/src/test/java/com/alibaba/demo/test/juc/ThreadPoolTest.java
+++ start/src/test/java/com/alibaba/demo/test/juc/ThreadPoolTest.java
@@ -1,9 +1,7 @@
 package com.alibaba.demo.test.juc;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -15,8 +13,9 @@ import java.util.stream.IntStream;
  * @Date 2021/11/3 8:36
  * @Version 0.1
  **/
-public class ThreadJoinTest {
+public class ThreadPoolTest {
     static List numbers = IntStream.range(0, 20).boxed().collect(Collectors.toList());
+    static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 2, 5000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(100), new ThreadPoolExecutor.AbortPolicy());
 
     public static void main(String[] args) throws Exception {
         Thread thread1 = new Thread(() -> {
@@ -30,14 +29,9 @@ public class ThreadJoinTest {
                 System.out.println(Thread.currentThread().getName() + ">>" + num);
             });
         });
+        threadPoolExecutor.execute(thread1);
+        threadPoolExecutor.execute(thread2);
 
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-
-        int poolSize = ForkJoinPool.commonPool().getPoolSize();
-        System.out.println("Pool size: " + poolSize);
+        threadPoolExecutor.shutdown();
     }
 }
