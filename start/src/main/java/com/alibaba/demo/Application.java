46712c5fff4759127767b4ce2dbc6f798e6f35b4
byg <byg184244735@126.com>
2021-12-12 15:59:57 +0800

    byg:update config

--- start/src/main/java/com/alibaba/demo/Application.java
+++ start/src/main/java/com/alibaba/demo/Application.java
@@ -1,6 +1,8 @@
 package com.alibaba.demo;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,12 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * @author byg
  */
+@Slf4j
 @SpringBootApplication
+@EnableDubbo
 @EnableDubboConfiguration
 @MapperScan("com.alibaba.demo.repository")
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("start finish");
     }
 }
